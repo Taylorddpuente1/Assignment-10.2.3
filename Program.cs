e68d1e4cc459412a0719f0ed5b9240d6ef4e55f0
Taylo <Taylo@MARIEL>
2025-06-13 15:31:14 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// find a string that starts and ends with specific characaters
+
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+        Console.WriteLine("Enter the starting character for the string: ");
+        char startchar = Console.ReadLine().ToUpper()[0];
+
+    Console.WriteLine("Enter the ending character for the string: ");
+        char endchar = Console.ReadLine().ToUpper()[0];
+
+    bool found = false;
+        foreach (string city in cities)
+        {
+            if (city.StartsWith(startchar.ToString()) && city.EndsWith(endchar.ToString()))
+            {
+                Console.WriteLine("The city starting with {0} and ending with {1} is: {2}", startchar, endchar, city);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+{
+    Console.WriteLine("No city found that starts with {0} and ends with {1}.", startchar, endchar);
+}
+    }
+}
